bb62e64097ee600f6d0bc08e390310f9a3a8cb03
松嵜義博 <tr-909@hotmail.co.jp>
2024-01-01 16:02:54 +0900

    更新

---
+++ SpectrumAnalyzer/SignalProcess/BpfBank.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SignalProcess {
+	[StructLayout(LayoutKind.Sequential, Pack = 16)]
+	public struct BpfBank {
+		public float La1;
+		public float La2;
+		public float Lb1;
+		public float Lb2;
+
+		public float Ra1;
+		public float Ra2;
+		public float Rb1;
+		public float Rb2;
+
+		public float PowerL;
+		public float PowerR;
+		public float PeakL;
+		public float PeakR;
+
+		public float Ka1;
+		public float Ka2;
+		public float Kb0;
+		public float RmsSpeed;
+	}
+}
