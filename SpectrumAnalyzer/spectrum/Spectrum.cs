897aee8295d5e85e70fa21f34fb3f0624a75b10d
mctr909 <tr-909@hotmail.co.jp>
2024-10-28 09:28:42 +0900

    Delete ハンディ研究.dio

--- SpectrumAnalyzer/spectrum/Spectrum.cs
+++ SpectrumAnalyzer/spectrum/Spectrum.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Spectrum {
-	public partial class Spectrum {
+	public partial class Spectrum : IDisposable {
 		/// <summary>トランスポーズ[半音]</summary>
 		public double Transpose { get; set; } = 0.0;
 
@@ -26,57 +27,41 @@ namespace Spectrum {
 		/// <summary>波形合成用ピーク</summary>
 		internal PeakBank[] PeakBanks { get; private set; } = new PeakBank[BANK_COUNT];
 
-		private readonly BPF_BANK[] mp_bpf_banks = new BPF_BANK[BANK_COUNT];
 		private readonly int m_sample_rate;
+		private unsafe BPF_BANK* mp_bpf_banks = null;
 
-		private class BPF_BANK {
+		private struct BPF_BANK {
+			public float k_b0;
+			public float k_a2;
+			public float k_a1;
+			public float delta;
 			public float l_b2;
 			public float l_b1;
 			public float l_a2;
 			public float l_a1;
-			public float l_ms;
 			public float r_b2;
 			public float r_b1;
 			public float r_a2;
 			public float r_a1;
-			public float r_ms;
-			public float k_b0;
-			public float k_a2;
-			public float k_a1;
-			public float sigma;
-
-			public BPF_BANK(int sample_rate, double frequency) {
-				var omega = 2 * Math.PI * frequency / sample_rate;
-				var alpha = GetAlpha(sample_rate, frequency);
-				var a0 = 1.0 + alpha;
-				k_b0 = (float)(alpha / a0);
-				k_a1 = (float)(-2.0 * Math.Cos(omega) / a0);
-				k_a2 = (float)((1.0 - alpha) / a0);
-				sigma = (float)GetAlpha(sample_rate / (2 * Math.PI), frequency);
-			}
-
-			private static double GetAlpha(double sample_rate, double frequency) {
-				var band_width = 1 + Math.Log(FREQ_AT_BANDWIDTH / frequency, 2.0);
-				if (band_width < 1.0) {
-					band_width = 1.0;
-				}
-				var omega = 2 * Math.PI * frequency / sample_rate;
-				var s = Math.Sin(omega);
-				var x = Math.Log(2) / 4 * band_width / 12.0 * omega / s;
-				var a = s * Math.Sinh(x);
-				a = Math.Min(1, a);
-				return a;
-			}
+			public float ms_l;
+			public float ms_r;
 		}
 
-		public Spectrum(int sample_rate) {
+		public unsafe Spectrum(int sample_rate) {
 			m_sample_rate = sample_rate;
+			mp_bpf_banks = (BPF_BANK*)Marshal.AllocHGlobal(BANK_COUNT * sizeof(BPF_BANK));
 			for (int ixB = 0; ixB < BANK_COUNT; ++ixB) {
-				var frequency = BASE_FREQ * Math.Pow(2.0, ((double)ixB - HALFTONE_CENTER) / OCT_DIV);
-				mp_bpf_banks[ixB] = new BPF_BANK(sample_rate, frequency);
+				var frequency = BASE_FREQ * Math.Pow(2.0, (double)ixB / OCT_DIV);
+				SetBPFCoef(mp_bpf_banks + ixB, sample_rate, frequency);
 				PeakBanks[ixB] = new PeakBank {
 					DELTA = frequency / sample_rate
 				};
+			}
+		}
+
+		public unsafe void Dispose() {
+			if (mp_bpf_banks != null) {
+				Marshal.FreeHGlobal((IntPtr)mp_bpf_banks);
 			}
 		}
 
@@ -91,27 +76,46 @@ namespace Spectrum {
 			ExtractPeak();
 		}
 
+		private unsafe static void SetBPFCoef(BPF_BANK* p_bank, int sample_rate, double frequency) {
+			var band_width = 1 + Math.Log(FREQ_AT_BANDWIDTH / frequency, 2.0);
+			if (band_width < 0.666) {
+				band_width = 0.666;
+			}
+			var omega = 2 * Math.PI * frequency / sample_rate;
+			var c = Math.Cos(omega);
+			var s = Math.Sin(omega);
+			var x = Math.Log(2) / 2 * band_width / 12.0 * omega / s;
+			var sh = s * Math.Sinh(x);
+			var a0 = 1 + sh;
+			Marshal.StructureToPtr(new BPF_BANK(), (IntPtr)p_bank, true);
+			p_bank->k_b0 = (float)(sh / a0);
+			p_bank->k_a1 = (float)(-2 * c / a0);
+			p_bank->k_a2 = (float)((1 - sh) / a0);
+			p_bank->delta = (float)(1.0 * frequency / sample_rate);
+		}
+
 		private unsafe void CalcMeanSquare(IntPtr p_input, int sample_count) {
-			float l_b2, l_b1, l_a2, l_a1, l_ms;
-			float r_b2, r_b1, r_a2, r_a1, r_ms;
-			float k_b0, k_a2, k_a1, sigma;
+			float l_b2, l_b1, l_a2, l_a1;
+			float r_b2, r_b1, r_a2, r_a1;
+			float ms_l, ms_r;
 			float b0, a0;
+			float k_b0, k_a2, k_a1, delta;
 			for (int ixB = 0; ixB < BANK_COUNT; ++ixB) {
-				var p_bank = mp_bpf_banks[ixB];
-				l_b2 = p_bank.l_b2;
-				l_b1 = p_bank.l_b1;
-				l_a2 = p_bank.l_a2;
-				l_a1 = p_bank.l_a1;
-				l_ms = p_bank.l_ms;
-				r_b2 = p_bank.r_b2;
-				r_b1 = p_bank.r_b1;
-				r_a2 = p_bank.r_a2;
-				r_a1 = p_bank.r_a1;
-				r_ms = p_bank.r_ms;
-				k_b0 = p_bank.k_b0;
-				k_a2 = p_bank.k_a2;
-				k_a1 = p_bank.k_a1;
-				sigma = p_bank.sigma;
+				var p_bank = (float*)(mp_bpf_banks + ixB);
+				k_b0 = *p_bank++;
+				k_a2 = *p_bank++;
+				k_a1 = *p_bank++;
+				delta = *p_bank++;
+				l_b2 = *p_bank++;
+				l_b1 = *p_bank++;
+				l_a2 = *p_bank++;
+				l_a1 = *p_bank++;
+				r_b2 = *p_bank++;
+				r_b1 = *p_bank++;
+				r_a2 = *p_bank++;
+				r_a1 = *p_bank++;
+				ms_l = *p_bank++;
+				ms_r = *p_bank;
 				var p_wave = (float*)p_input;
 				for (int ixS = sample_count; ixS != 0; --ixS) {
 					/*** [左チャンネル] ***/
@@ -125,10 +129,10 @@ namespace Spectrum {
 					l_b1 = b0;
 					l_a2 = l_a1;
 					l_a1 = a0;
-					/* 二乗平均 */
+					/* 振幅の二乗平均 */
 					a0 *= a0;
-					a0 -= l_ms;
-					l_ms += a0 * sigma;
+					a0 -= ms_l;
+					ms_l += a0 * delta;
 					/*** [右チャンネル] ***/
 					/* 帯域通過フィルタ */
 					b0 = *p_wave++;
@@ -140,29 +144,30 @@ namespace Spectrum {
 					r_b1 = b0;
 					r_a2 = r_a1;
 					r_a1 = a0;
-					/* 二乗平均 */
+					/* 振幅の二乗平均 */
 					a0 *= a0;
-					a0 -= r_ms;
-					r_ms += a0 * sigma;
+					a0 -= ms_r;
+					ms_r += a0 * delta;
 				}
-				p_bank.l_b2 = l_b2;
-				p_bank.l_b1 = l_b1;
-				p_bank.l_a2 = l_a2;
-				p_bank.l_a1 = l_a1;
-				p_bank.l_ms = l_ms;
-				p_bank.r_b2 = r_b2;
-				p_bank.r_b1 = r_b1;
-				p_bank.r_a2 = r_a2;
-				p_bank.r_a1 = r_a1;
-				p_bank.r_ms = r_ms;
+				*p_bank-- = ms_r;
+				*p_bank-- = ms_l;
+				*p_bank-- = r_a1;
+				*p_bank-- = r_a2;
+				*p_bank-- = r_b1;
+				*p_bank-- = r_b2;
+				*p_bank-- = l_a1;
+				*p_bank-- = l_a2;
+				*p_bank-- = l_b1;
+				*p_bank = l_b2;
 			}
 		}
 
-		private void UpdateAutoGain(int sample_count) {
+		private unsafe void UpdateAutoGain(int sample_count) {
 			/* 最大値を更新 */
 			Max = AUTOGAIN_MIN;
-			foreach (var bank in mp_bpf_banks) {
-				var amp = Math.Sqrt(Math.Max(bank.l_ms, bank.r_ms) * 2);
+			for (int ixB = 0; ixB < BANK_COUNT; ++ixB) {
+				var b = mp_bpf_banks[ixB];
+				var amp = Math.Sqrt(Math.Max(b.ms_l, b.ms_r) * 2);
 				Max = Math.Max(Max, amp);
 			}
 
@@ -176,13 +181,7 @@ namespace Spectrum {
 			}
 		}
 
-		private void ExtractPeak() {
-			var last_amp_l = 0.0;
-			var last_amp_r = 0.0;
-			var last_amp_disp = 0.0;
-			var last_index_l = -1;
-			var last_index_r = -1;
-			var last_index_disp = -1;
+		private unsafe void ExtractPeak() {
 			for (int ixB = 0; ixB < BANK_COUNT; ++ixB) {
 				/*** ピーク抽出用の閾値を算出 ***/
 				var threshold_l = 0.0;
@@ -199,88 +198,56 @@ namespace Spectrum {
 					} else {
 						width = THRESHOLD_WIDTH_HIGH;
 					}
-					/* 閾値幅で指定される範囲のスペクトルの平均値を閾値にする */
+					/* 閾値幅で指定される範囲の最大値を閾値とする */
+					/* 平均値を閾値の下限とする */
+					var ms_l = 0.0;
+					var ms_r = 0.0;
 					for (int ixW = -width; ixW <= width; ++ixW) {
 						var bw = Math.Min(BANK_COUNT - 1, Math.Max(0, ixB + ixW));
 						var b = mp_bpf_banks[bw];
-						threshold_l += b.l_ms * b.l_ms;
-						threshold_r += b.r_ms * b.r_ms;
+						ms_l += b.ms_l;
+						ms_r += b.ms_r;
+						threshold_l = Math.Max(threshold_l, b.ms_l);
+						threshold_r = Math.Max(threshold_r, b.ms_r);
 					}
-					width = width * 2 + 1;
-					var scale = 4.0 / width;
-					threshold_l = Math.Pow(threshold_l * scale, 0.25) * THRESHOLD_GAIN;
-					threshold_r = Math.Pow(threshold_r * scale, 0.25) * THRESHOLD_GAIN;
+					width <<= 1;
+					width++;
+					double ms_scale;
+					if (ixB < BEGIN_HIGH) {
+						ms_scale = 1.047; //+0.2db
+					} else {
+						ms_scale = 1.188; //+0.75db
+					}
+					ms_scale /= width;
+					ms_l *= ms_scale;
+					ms_r *= ms_scale;
+					threshold_l = Math.Max(threshold_l, ms_l);
+					threshold_r = Math.Max(threshold_r, ms_r);
+					/* 2乗平均を振幅に変換 */
+					threshold_l = Math.Sqrt(threshold_l * 2);
+					threshold_r = Math.Sqrt(threshold_r * 2);
 				}
-				var bank = mp_bpf_banks[ixB];
 				/*** 波形合成用のピークを抽出 ***/
-				{
-					var p_peak = PeakBanks[ixB];
-					p_peak.L = 0.0;
-					p_peak.R = 0.0;
-					var amp_l = Math.Sqrt(bank.l_ms * 2);
-					var amp_r = Math.Sqrt(bank.r_ms * 2);
-					if (amp_l < threshold_l) {
-						if (0 <= last_index_l) {
-							PeakBanks[last_index_l].L = last_amp_l;
-						}
-						amp_l = 0.0;
-						last_amp_l = 0.0;
-						last_index_l = -1;
-					}
-					if (last_amp_l < amp_l) {
-						last_amp_l = amp_l;
-						last_index_l = ixB;
-					}
-					if (amp_r < threshold_r) {
-						if (0 <= last_index_r) {
-							PeakBanks[last_index_r].R = last_amp_r;
-						}
-						amp_r = 0.0;
-						last_amp_r = 0.0;
-						last_index_r = -1;
-					}
-					if (last_amp_r < amp_r) {
-						last_amp_r = amp_r;
-						last_index_r = ixB;
-					}
+				var bank = mp_bpf_banks[ixB];
+				var amp_l = Math.Sqrt(bank.ms_l * 2);
+				var amp_r = Math.Sqrt(bank.ms_r * 2);
+				var p_peak = PeakBanks[ixB];
+				p_peak.L = amp_l < threshold_l ? 0.0 : amp_l;
+				p_peak.R = amp_r < threshold_r ? 0.0 : amp_r;
+				/*** 表示用のピークを抽出、曲線と閾値を設定 ***/
+				var amp = Math.Max(amp_l, amp_r);
+				var threshold = Math.Max(threshold_l, threshold_r);
+				if (EnableNormalize) {
+					amp /= Max;
+					threshold /= Max;
 				}
-				/*** 表示用のピークを抽出、曲線を設定 ***/
-				{
-					var amp = Math.Sqrt(Math.Max(bank.l_ms, bank.r_ms) * 2);
-					var threshold = Math.Max(threshold_l, threshold_r);
-					if (EnableNormalize) {
-						amp /= Max;
-						threshold /= Max;
-					}
-					if (EnableAutoGain) {
-						amp /= AutoGain;
-						threshold /= AutoGain;
-					}
-					Curve[ixB] = amp;
-					Peak[ixB] = 0.0;
-					Threshold[ixB] = threshold;
-					if (amp < threshold) {
-						if (0 <= last_index_disp) {
-							Peak[last_index_disp] = last_amp_disp;
-						}
-						amp = 0.0;
-						last_amp_disp = 0.0;
-						last_index_disp = -1;
-					}
-					if (last_amp_disp < amp) {
-						last_amp_disp = amp;
-						last_index_disp = ixB;
-					}
+				if (EnableAutoGain) {
+					amp /= AutoGain;
+					threshold /= AutoGain;
 				}
-			}
-			if (0 <= last_index_l) {
-				PeakBanks[last_index_l].L = last_amp_l;
-			}
-			if (0 <= last_index_r) {
-				PeakBanks[last_index_r].R = last_amp_r;
-			}
-			if (0 <= last_index_disp) {
-				Peak[last_index_disp] = last_amp_disp;
+				Curve[ixB] = amp;
+				Threshold[ixB] = threshold;
+				Peak[ixB] = amp < threshold ? 0.0 : amp;
 			}
 		}
 	}
